07f4653a9ca3185e7c5eba9742f5530648412593
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2022-06-01 21:33:49 +0300

    adds Badge widget

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'MyShop',
         theme: ThemeData(
           fontFamily: "Lato",
