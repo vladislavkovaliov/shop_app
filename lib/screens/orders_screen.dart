fa689d429454cd0743fbfbaa176ffef3258d700e
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2022-06-12 13:50:24 +0300

    uses FutureBuilder instead of Future.delayed(0)

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders/orders.dart' show Orders;
-import 'package:shop_app/screens/products_overview_screen.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
@@ -15,56 +14,46 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
+  Future? _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
 
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) {
-      setState(() {
-        _isLoading = true;
-      });
-      Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      }).catchError((error) {
-        showDialog(
-          context: context,
-          builder: (ctx) => AlertDialog(
-            title: const Text('An error occurred!'),
-            content: Text(error.toString()),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .pushNamed(ProductOverviewScreen.routeName);
-                },
-                child: const Text('Okay'),
-              ),
-            ],
-          ),
-        );
-      });
-    });
+    _ordersFuture = _obtainOrdersFuture();
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orders = Provider.of<Orders>(context);
-
     return Scaffold(
       appBar: AppBar(title: const Text("Your Orders")),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemBuilder: (ctx, i) {
-                return OrderItem(
-                  order: orders.orders[i],
-                );
-              },
-              itemCount: orders.orders.length,
-            ),
+      body: FutureBuilder(
+        future: _ordersFuture,
+        builder: (ctx, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return const Center(child: CircularProgressIndicator());
+
+            case ConnectionState.done:
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemBuilder: (ctx, i) {
+                    return OrderItem(
+                      order: orderData.orders[i],
+                    );
+                  },
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            default:
+              return const Center(child: Text('An error occured.'));
+          }
+        },
+      ),
       drawer: const AppDrawer(),
     );
   }
